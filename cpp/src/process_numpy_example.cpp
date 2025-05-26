7f2db1059ad764ff88d2e39be269e02fef8cfbf8
Aum Patel <aumpatel131@gmail.com>
2025-05-26 09:24:19 +0530

    test for np.dtype

--- cpp/src/process_numpy_example.cpp
+++ cpp/src/process_numpy_example.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <pybind11/buffer_info.h>
 #include <pybind11/detail/common.h>
@@ -8,14 +9,29 @@
 
 namespace py = pybind11;
 
-void print_array(py::array_t<double> ndarray) {
+void print_array(py::array ndarray) {
   pybind11::buffer_info buffer = ndarray.request();
-  double *ptr = static_cast<double *>(buffer.ptr);
+  auto dtype = py::str(ndarray.dtype()).cast<std::string>();
+  std::cout << "ndarray reading via c++!" << std::endl;
+  std::cout << "dtype - " << dtype << std::endl;
   std::size_t size = buffer.size;
 
-  std::cout << "ndarray reading via c++ : ";
-  for (int i = 0; i < size; i++) {
-    std::cout << ptr[i] << " ";
+  if (dtype == "int8") {
+    int8_t *ptr = static_cast<int8_t *>(buffer.ptr);
+    for (size_t i = 0; i < size; i++)
+      std::cout << (int)ptr[i] << " "; // cast to int for readable output
+  } else if (dtype == "int32") {
+    int32_t *ptr = static_cast<int32_t *>(buffer.ptr);
+    for (size_t i = 0; i < size; i++)
+      std::cout << ptr[i] << " ";
+  } else if (dtype == "float32") {
+    float *ptr = static_cast<float *>(buffer.ptr);
+    for (size_t i = 0; i < size; i++)
+      std::cout << ptr[i] << " ";
+  } else {
+    double *ptr = static_cast<double *>(buffer.ptr);
+    for (size_t i = 0; i < size; i++)
+      std::cout << ptr[i] << " ";
   }
   std::cout << std::endl;
 }
--- tests/examples/np_example.py
+++ tests/examples/np_example.py
@@ -6,5 +6,6 @@ sys.path.append(os.path.abspath("../../cpp/build"))
 
 import mlcore_cpp
 
-arr = np.array([1.0, 2.0, 3.0, 4.0])
+arr = np.array([1.0, 2.0, 3.0, 4.0], dtype=np.int32)
+print(arr.dtype)
 mlcore_cpp.print_array_eg(arr)
